2a4e8a453d245a939068633e426906ea14c1e910
amitsawant1234 <amitmilindsawant1993@gmail.com>
2017-09-23 20:43:57 +0530

    forent end

---
+++ PageController.java
@@ -0,0 +1,20 @@
+package WebProjectMaven.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+
+	@RequestMapping(value = { "/", "/home", "/index" })
+	public ModelAndView index() {
+
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("welcome", "Welcome to spring Web MVC");
+		return mv;
+	}
+
+	
+	
+}
